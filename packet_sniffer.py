9c2903fc52b9275f84a67bce488060e071ccdb2d
LeuvaApurv <57341831+LeuvaApurv@users.noreply.github.com>
2020-02-28 14:46:49 +0530

    Update packet_sniffer.py

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -4,6 +4,7 @@ import scapy.all as scapy
 from scapy.layers import http
 
 def sniff(interface):
+    print("\n\033[91m[-] Exit from This proocess to Press CTRL + C\n")
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 def get_url(packet):
